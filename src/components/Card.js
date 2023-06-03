a04758d5793b8f6fba2b8abeac80e927f67375c6
Cazacu Alin <alincazacu9999@gmail.com>
2023-06-03 12:59:43 +0300

    delete comments

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,7 @@ const Card = ({name, email, id}) => {
  
   
   return (
-     //tachyon classes//
+  
     <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'> 
       <div>
         <img alt='robots' src={`https://robohash.org/${id}?200X200`}/>
